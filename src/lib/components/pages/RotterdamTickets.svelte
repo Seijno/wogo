f4b73def12ffac7dfb7bd515e3c088b2822f93df
Latoya <76912011+latoyaln@users.noreply.github.com>
2024-10-09 06:22:45 +0200

    #6 hero styling changed
    Nu komt de tekst op de hero. Bij de vorige commit was de tekst niet te zien

--- src/lib/components/pages/RotterdamTickets.svelte
+++ src/lib/components/pages/RotterdamTickets.svelte
@@ -3,12 +3,6 @@
     export let items
   </script>
   
-  <section>
-    <section>
-      <h1>Rotterdam Tickets</h1>
-    </section>
-  </section>
-  
   <svelte:head>
     <title></title>
   </svelte:head>
